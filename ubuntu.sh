2c0bf13db25b6fa2d6d59af425e088698f2cdcf9
vvjadhav <65950660+vvjadhav@users.noreply.github.com>
2024-06-30 20:34:04 +0530

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -3,4 +3,4 @@
 export DEBIAN_FRONTEND=noninteractive
 apt update -y
 apt upgrade -y
-apt install net-tools open-vm-tools iputils-ping traceroute sshpass tcpdump vim git -y
+apt install net-tools open-vm-tools iputils-ping traceroute sshpass tcpdump vim git pssh python3-pip -y
